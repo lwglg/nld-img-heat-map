63c59b98bb5862b30f442ba94134f0e13305b377
Guilherme Lima Gonçalves <lwglguilherme@gmail.com>
2025-06-29 02:28:51 -0300

    feat: implement application core

---
+++ src/core/pydantic.py
@@ -0,0 +1,21 @@
+from pydantic import ConfigDict
+from pydantic.fields import ModelField
+
+
+class ConStr(str):
+    min_length = 0
+    max_length = 0
+
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, value: str, field: ModelField, config: ConfigDict):
+        if not isinstance(value, str):
+            raise TypeError('This value is only str')
+
+        if not cls.min_length <= len(value) <= cls.max_length:
+            raise ValueError(f'This value length {cls.min_length} ~ {cls.max_length}')
+
+        return value
