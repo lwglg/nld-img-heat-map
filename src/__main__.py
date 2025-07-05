3c6f63ab40f4d204cc1089ffc84984e0344a707d
Guilherme Lima Gonçalves <lwglguilherme@gmail.com>
2025-07-04 21:53:30 -0300

    fix: integrate new settings object into application factory

--- src/__main__.py
+++ src/__main__.py
@@ -17,13 +17,14 @@ if __name__ == "__main__":
             .get_instance()
 
         if app is None:
-            raise Exception("FastAPI application instance not constructed properly. Not possible to run AWSGI server.")
+            raise Exception("FastAPI application instance not constructed properly. Not possible to run ASGI server.")
 
         uvicorn.run(
             app,
-            host=WEBSERVICE_SETTINGS["host"],
-            port=WEBSERVICE_SETTINGS["port"],
-            log_level=WEBSERVICE_SETTINGS["log_level"]
+            host=WEBSERVICE_SETTINGS.host,
+            port=WEBSERVICE_SETTINGS.port,
+            log_level=WEBSERVICE_SETTINGS.log_level,
+            lifespan="on"
         )
     except Exception as exc:
         logger.error(f"{exc.__class__.__name__}: {str(exc)}")
